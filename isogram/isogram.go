723f3db5611546a8fc878cd4dba2963d0f49d830
EconClass <bizmail327@gmail.com>
2019-01-30 21:48:25 -0800

    fixed

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,7 +1,6 @@
 package isogram
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
 )
@@ -13,33 +12,19 @@ func IsIsogram(text string) bool {
 	compare := ""
 
 	cleaned := strings.ToLower(text)
-	// fmt.Println(cleaned)
 
 	regex := regexp.MustCompile("[^a-zA-Z]+")
 
 	cleaned = regex.ReplaceAllString(cleaned, "")
-	// fmt.Println(cleaned)
 
-	// loop through all elements in phrase
 	for _, letter := range cleaned {
-		// fmt.Println(!strings.Contains(compare, cleaned))
-		if strings.Contains(string(letter), cleaned) {
-			// fmt.Println(letter)
+		if !strings.Contains(compare, string(letter)) {
 			compare = compare + string(letter)
-			fmt.Println(compare)
 		}
-		fmt.Println(compare)
 	}
-
-	// fmt.Println(cleaned)
-	// fmt.Println(compare)
 
 	if cleaned == compare {
 		response = true
 	}
 	return response
 }
-
-// func main() {
-// 	IsIsogram("isogram")
-// }
