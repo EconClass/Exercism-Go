40a86880a2d441c8d252b9a02e7a8595d6494180
EconClass <bizmail327@gmail.com>
2019-02-07 11:40:09 -0800

    fixed checks

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,8 +1,7 @@
-package main
+package luhn
 
 import (
 	"fmt"
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -10,41 +9,40 @@ import (
 // Valid checks if a number is valid according to the Luhn algorithm
 func Valid(input string) bool {
 	check := false
-	val := strings.TrimSpace(input)
+	val := strings.Replace(input, " ", "", -1)
 
-	reg := regexp.MustCompile("\\D")
-	val = reg.ReplaceAllString(val, "")
+	if val == "0"{
+		return false
+	}
+
 	slice := make([]int, 0)
+	fmt.Println(slice)
 	for i := range val {
-		add, _ := strconv.Atoi(string(val[i]))
+		add, err := strconv.Atoi(string(val[i]))
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
 		slice = append(slice, add)
 	}
-	fmt.Println(slice)
-	// The first step of the Luhn algorithm is to double every second digit, starting from the right
-	// If doubling the number results in a number greater than 9 then subtract 9 from the product
-	// Then sum all of the digits; if the sum is evenly divisible by 10, then the number is valid
-	for i := len(slice) - 1; i <= 0; i-- {
-		fmt.Println(slice[i])
+
+	
+	for i := len(slice) - 2; i >= 0; i = i - 2 {
 		slice[i] = slice[i] * 2
-		fmt.Println(slice[i])
+		
 		if slice[i] > 9 {
 			slice[i] = slice[i] - 9
 		}
 	}
-	fmt.Println(slice)
+	
 	sum := 0
 	for i := range slice {
 		sum += slice[i]
 	}
-	fmt.Println(sum)
 
-	if sum != 0 && sum%10 == 0 {
+	if sum%10 == 0 {
 		check = true
 	}
 
 	return check
-}
-
-func main() {
-	fmt.Println(Valid("6103017551"))
 }
