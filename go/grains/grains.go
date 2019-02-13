49c08155ba456c32d871ab8581514ba64150c4f8
EconClass <bizmail327@gmail.com>
2019-02-12 21:33:59 -0800

    cleaned grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -7,10 +7,7 @@ import (
 func makeBoard(max int) []uint64 {
 	slice := make([]uint64, 64)
 	for i := range slice {
-		slice[i] = uint64(i+1)
-		if slice[i] > 2 {
-			slice[i] = slice[i-1] << 1
-		}
+		slice[i] = 1 << uint64(i)
 	}
 	return slice
 }
@@ -19,9 +16,6 @@ func Square(index int) (uint64, error) {
 	if index < 1 || index > 64 {
 		return 0, errors.New("Pick a number from 1 to 64")
 	}
-	if index <= 2 {
-		return uint64(index), nil
-	}
 	return 1 << uint64(index-1), nil
 }
 
@@ -29,8 +23,8 @@ func Total() uint64{
 	var board []uint64
 	board = makeBoard(64)
 	var sum uint64 = 0
-	for i := range board {
-		sum += board[i]
+	for _, grains := range board {
+		sum += grains
 	}
 	return sum
 }
