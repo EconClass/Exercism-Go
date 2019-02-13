7bca28b8c34e9536b7776b062f2de5b588d7e2a5
EconClass <bizmail327@gmail.com>
2019-02-12 21:01:15 -0800

    grains Square optimized

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,30 +1,36 @@
-package main
+package grains
 
 import (
-	"fmt"
-	// "math"
+	"errors"
 )
 
 func makeBoard(max int) []uint64 {
 	slice := make([]uint64, 64)
 	for i := range slice {
 		slice[i] = uint64(i+1)
-	}
-
-	for i := range slice {
-		if slice[i] > 2{
+		if slice[i] > 2 {
 			slice[i] = slice[i-1] << 1
 		}
 	}
-
 	return slice
 }
 
-func Square(index int) uint64 {
-	fmt.Println(makeBoard(index))
-	return 0
+func Square(index int) (uint64, error) {
+	if index < 1 || index > 64 {
+		return 0, errors.New("Pick a number from 1 to 64")
+	}
+	if index <= 2 {
+		return uint64(index), nil
+	}
+	return 1 << uint64(index-1), nil
 }
 
-func main() {
-	Square(64)
+func Total() uint64{
+	var board []uint64
+	board = makeBoard(64)
+	var sum uint64 = 0
+	for i := range board {
+		sum += board[i]
+	}
+	return sum
 }
