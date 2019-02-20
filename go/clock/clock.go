4fa00bc732792cf5d5513995143a461affb6f8a7
EconClass <bizmail327@gmail.com>
2019-02-19 16:57:25 -0800

    passed all tests

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -16,13 +16,35 @@ type Clock struct {
 
 // New Returns a new Clock type given the hours and minutes field
 func New(hours, minutes int) Clock {
+	for minutes >= 60 {
+		hours++
+		minutes = minutes - 60
+	}
+	for minutes < 0 {
+		hours--
+		minutes = minutes + 60
+	}
+	for hours > 23 {
+		hours = hours - 24
+	}
+	for hours < 0 {
+		hours = hours + 24
+	}
+
 	return Clock{hours, minutes}
 }
 
 func (c Clock) String() string {
-	result := "%v:%v"
-	if c.hours > 9 && c.minutes > 9 {
-		return fmt.Sprintf(result, c.hours, c.minutes)
+	result := ""
+	switch {
+	case c.hours > 9 && c.minutes > 9:
+		result = "%v:%v"
+	case c.hours > 9 && c.minutes < 10:
+		result = "%v:0%v"
+	case c.hours < 10 && c.minutes > 9:
+		result = "0%v:%v"
+	default:
+		result = "0%v:0%v"
 	}
 	return fmt.Sprintf(result, c.hours, c.minutes)
 }
@@ -30,13 +52,17 @@ func (c Clock) String() string {
 // Add adds minutes to an existing Clock type
 func (c Clock) Add(add int) Clock {
 	c.minutes = c.minutes + add
-	fmt.Println(c.minutes)
+	// fmt.Println(c.minutes)
 	addHours := 0
-	for c.minutes > 60 {
+	for c.minutes >= 60 {
 		c.minutes = c.minutes - 60
 		addHours++
 	}
+	// fmt.Println(c.minutes)
 	c.hours = c.hours + addHours
+	for c.hours > 23 {
+		c.hours = c.hours - 24
+	}
 	return c
 }
 
@@ -49,6 +75,9 @@ func (c Clock) Subtract(subtract int) Clock {
 		removeHours--
 	}
 	c.hours = c.hours + removeHours
+	for c.hours < 0 {
+		c.hours = c.hours + 24
+	}
 	return c
 }
 
@@ -61,6 +90,7 @@ func CompareClocks(first, second Clock) bool {
 }
 
 // func main() {
-// 	clock := New(8, 0)
-// 	// fmt.Println(clock.String())
+// 	clock := New(10, 30)
+// 	clock.Add(30)
+// 	fmt.Println(clock.String())
 // }
