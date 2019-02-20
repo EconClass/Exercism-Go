724ffbf4f48c0b5c11481480913c1f797b7b0a35
EconClass <bizmail327@gmail.com>
2019-02-20 14:22:10 -0800

    synchronous version works

--- go/parallel-letter-frequency/frequency.go
+++ go/parallel-letter-frequency/frequency.go
@@ -12,3 +12,29 @@ func Frequency(s string) FreqMap {
 	}
 	return m
 }
+
+// ConcurrentFrequency crates a map of unique runes as keys and their respective
+// frequencies as values using a concurrent design.
+func ConcurrentFrequency(slc []string) FreqMap {
+	m := FreqMap{}
+
+	func(s1 string) {
+		for _, r := range s1 {
+			m[r]++
+		}
+	}(slc[0])
+
+	func(s2 string) {
+		for _, r := range s2 {
+			m[r]++
+		}
+	}(slc[1])
+
+	func(s3 string) {
+		for _, r := range s3 {
+			m[r]++
+		}
+	}(slc[2])
+
+	return m
+}
